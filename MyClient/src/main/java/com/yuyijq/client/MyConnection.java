824847046b0384f00e4c71d2f762ffb6b2d2ea01
yuyijq <yuyijq@hotmail.com>
2012-02-05 00:22:02 +0800

    Removed unused methods.

--- MyClient/src/main/java/com/yuyijq/client/MyConnection.java
+++ MyClient/src/main/java/com/yuyijq/client/MyConnection.java
@@ -1,20 +1,13 @@
 package com.yuyijq.client;
 
-import java.util.ArrayList;
-import java.util.EventListener;
-import java.util.List;
-
 public class MyConnection {
     private String[] urls;
     private DriverFactory driverFactory;
-
-    private List<EventListener> connectedListeners;
 
     public MyConnection(String[] urls, DriverFactory driverFactory) {
         this.urls = urls;
         this.driverFactory = driverFactory;
 
-        connectedListeners = new ArrayList<EventListener>();
     }
 
     public void open() {
@@ -27,7 +20,4 @@ public class MyConnection {
         thread.start();
     }
 
-    public void addConnectedListener(ConnectedListener connectedListener) {
-        connectedListeners.add(connectedListener);
-    }
 }
